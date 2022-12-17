4cb2fbf17d5740730f17e8170bc0a4d45209b5ac
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-17 12:44:03 +0200

    how old are you?

---
+++ src/main/scala/HowOldAreYou.scala
@@ -0,0 +1,12 @@
+import scala.io.StdIn.readInt
+
+/*
+Write a program that will read the user's age and print it to the console with the line "Your age is".
+For example, if the user's age is 18, the program should print "Your age is 18".
+*/
+
+object HowOldAreYou {
+  def main(args: Array[String]): Unit = {
+    println(s"Your age is ${readInt()}")
+  }
+}
