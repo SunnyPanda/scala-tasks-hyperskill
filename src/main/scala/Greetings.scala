4784acced1ff58942d4d8f61eb84ebe5cf424ecb
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-17 12:30:45 +0200

    greetings

---
+++ src/main/scala/Greetings.scala
@@ -0,0 +1,9 @@
+/* Modify the program below to output greetings for the user with the name received by input.
+   Note that the mkString method concatenates all list elements to a string with the separator as an argument.
+*/
+
+object Greetings extends App {
+  val name = scala.io.StdIn.readLine()
+  val list = "Hello, " :: name :: "!" :: Nil
+  println(list.mkString(""))
+}
