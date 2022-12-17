cad8b1398074eab554311db3fb573d994af31a95
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-17 12:40:46 +0200

    palindrome

---
+++ src/main/scala/PalindromeChecker.scala
@@ -0,0 +1,9 @@
+/*
+Modify the program below so that it outputs true or false depending on whether the input string is a palindrome or not.
+*/
+
+object PalindromeChecker extends App {
+  val s = scala.io.StdIn.readLine()
+  val onlyLowerCaseLetters = s.filter(_.isLetter).map(_.toLower)
+  println(onlyLowerCaseLetters == onlyLowerCaseLetters.reverse)
+}
