c93c45ad74770a007652feea888b7d5c50ee83c4
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-17 12:45:08 +0200

    summation

---
+++ src/main/scala/Summation.scala
@@ -0,0 +1,13 @@
+import scala.io.StdIn.readInt
+
+/*
+Write a program that reads 2 numbers from the console and displays their sum.
+*/
+
+object Summation {
+  def main(args: Array[String]): Unit = {
+    val first = readInt()
+    val second = readInt()
+    print(first + second)
+  }
+}
