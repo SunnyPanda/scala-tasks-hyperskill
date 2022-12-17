d77a9e672db364d07266b5b42cd236014b9cbbbf
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-17 12:39:26 +0200

    text statistics

---
+++ src/main/scala/TextReportPrinter.scala
@@ -0,0 +1,14 @@
+/*
+Modify the program below to output the statistics of the input text line:
+the number of characters, unique characters, and words.
+*/
+
+object TextReportPrinter extends App {
+
+  val textLine = scala.io.StdIn.readLine()
+  println(
+    s"""characters: ${textLine.length}
+       |unique characters: ${textLine.toSet.size}
+       |words: ${if (textLine.isEmpty) 0 else textLine.split("\\s").length}
+       |""".stripMargin)
+}
