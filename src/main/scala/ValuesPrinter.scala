5aa57b1d9f0dae872e04005705f52057f7ea28a5
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-17 12:24:01 +0200

    values printer

---
+++ src/main/scala/ValuesPrinter.scala
@@ -0,0 +1,9 @@
+// Modify a program by defining values a, b, c, d to print 123.456.
+
+object ValuesPrinter extends App {
+  val a = 100.006
+  val b = 20.05
+  val c = 3.4
+  val d = 0
+  println(a + b + c + d)
+}
