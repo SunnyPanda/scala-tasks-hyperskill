aad1555fee2de4fe3ea2a2d60462bdfd9f884758
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-17 12:33:54 +0200

    planets game

---
+++ src/main/scala/PlanetsGame.scala
@@ -0,0 +1,13 @@
+/*
+Scally is newbie programmer. She tries to entertain herself with some astronomy.
+She started writing a program that prints true if the input lines are real planets of the Solar system
+and false otherwise. Finish her program.
+ */
+
+object PlanetsGame extends App {
+  val set = Set("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+  val first = scala.io.StdIn.readLine()
+  val second = scala.io.StdIn.readLine()
+  val third = scala.io.StdIn.readLine()
+  println(set(first) && set(second) && set(third))
+}
---
+++ src/main/scala/RockPaperScissors.scala
@@ -0,0 +1,32 @@
+/*
+Finish the program that implements the famous game Rock paper scissors.
+This is a game for two players where each selects "rock", "paper" or "scissors".
+Our program receives the players' choices as input lines: for the first player and for the second.
+If both select the same shape, the program prints draw , otherwise it prints who wins.
+ */
+
+object RockPaperScissors extends App {
+  val gameMap = Map(
+    "rock" -> Map(
+      "paper" -> "second win",
+      "scissors" -> "first win"
+    ),
+    "paper" -> Map(
+      "scissors" -> "second win",
+      "rock" -> "first win"
+    ),
+    "scissors" -> Map(
+      "rock" -> "second win",
+      "paper" -> "first win"
+    )
+  )
+
+  val list = List(scala.io.StdIn.readLine(), scala.io.StdIn.readLine())
+  val set = Set(list.head, list(1))
+  if (set.size == 1)
+    println("draw")
+  else {
+    val firstMap = gameMap(list.head)
+    println(firstMap(list.tail.head))
+  }
+}
