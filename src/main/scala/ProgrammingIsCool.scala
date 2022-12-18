853ba7087d7395735de5ebe7875d597a03300c76
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-18 18:02:07 +0200

    programming is cool

---
+++ src/main/scala/ProgrammingIsCool.scala
@@ -0,0 +1,10 @@
+/*
+ * Define a constant variable named isProgrammingCool so that the following code that uses this variable
+ * prints the correct dialog.
+ */
+object ProgrammingIsCool extends App {
+  /* your code */
+  val isProgrammingCool = true
+  println("— Programming is cool.")
+  println(s"— So $isProgrammingCool!")
+}
