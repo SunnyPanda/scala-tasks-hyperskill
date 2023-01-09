e9e6ad2806afe2d66cd63b6ee63bba85518f2bd1
SunnyPanda <kashtanka.nya@gmail.com>
2023-01-09 22:06:54 +0200

    Imports -> Minimum of minimums

--- src/main/scala/Import.scala
+++ src/main/scala/Import.scala
@@ -1,4 +1,18 @@
+import scala.collection.mutable
 import scala.math._
+//import scala.math.min
 import scala.math.{E => EulerNumber}
 
 class Import
+
+object MyWorld {
+  import scala.collection.mutable
+  val animals: mutable.Seq[String] = mutable.Seq("cat", "dog", "frog")
+}
+
+object AnotherWorld {
+  val birds: mutable.Seq[String] = mutable.Seq("pigeon", "flamingo")
+}
+
+def minOf3(n1: Int, n2: Int, n3: Int): Int =
+  min(min(n1, n2), n3)
