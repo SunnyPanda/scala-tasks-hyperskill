f2a31c17ad0c5b66eeb0ffd9fd2ad4927ba278a7
SunnyPanda <kashtanka.nya@gmail.com>
2023-01-13 20:38:24 +0200

    Objects and package objects -> Launch a Scala application with an object

---
+++ src/main/scala/Launch.scala
@@ -0,0 +1,7 @@
+/*
+ * Create an object that will be inherited from the App trait.
+ * This object should print Hello hyperskill! during startup.
+ */
+class Launch extends App {
+  println("Hello hyperskill!")
+}
